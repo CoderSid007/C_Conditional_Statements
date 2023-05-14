1f033666ee821fc2a070cff889e55106bb0bb225
CoderSid007 <siddhesdas2020@gmail.com>
2023-05-14 17:27:06 +0530

    first commit

---
+++ positive_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+    int num1;
+
+
+    printf("enter the first number:");
+    scanf("%d",&num1);
+
+
+
+    if(num1>= 0)
+    {
+        printf("it is a positive number");
+    }
+    else
+    {
+        printf("it is a negative number");
+    }
+    return 0;
+
+
+}
