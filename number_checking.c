25e0f786909bd1ce646a4ee30dfa9e8368c27d34
CoderSid007 <siddhesdas2020@gmail.com>
2023-05-14 18:25:46 +0530

    4th commit

---
+++ number_checking.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int n , m;
+
+
+    printf("enter the number:");
+    scanf("%d",&m);
+
+
+    
+
+    if(m > 0)
+    {
+        n = 1;
+        printf("value of n is %d", n);
+    }
+    if(m = 0)
+    {
+        n = 0;
+        printf("value of n is %d",n);
+    }
+    if(m < 0)
+    {
+        n = -1;
+        printf("value of n is %d",n);
+    }
+   
+    return 0;
+
+
+}
