a70a9e1b1a031b317124afeea6de52b405b9a37c
CoderSid007 <siddhesdas2020@gmail.com>
2023-05-15 18:55:42 +0530

    19th commit

---
+++ day_display_using_switch_case.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void main()
+{
+    int day;
+
+    printf("Enter the day number:");
+    scanf("%d",&day);
+
+    switch(day)
+    {
+        case 0:
+        printf("SUNDAY");
+        break;
+
+        case 1:
+        printf("MONDAY");
+        break;
+
+        case 2:
+        printf("TUESDAY");
+        break;
+
+        case 3:
+        printf("WEDNESDAY");
+        break;
+
+        case 4:
+        printf("THURSDAY");
+        break;
+
+        case 5:
+        printf("FRIDAY");
+        break;
+
+        case 6:
+        printf("SATURDAY");
+        break;
+    }
+    return 0;
+
+
+
+}
